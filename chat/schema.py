eb642f2b652d318809183cdac0eb8e2c992700c2
D4C <akbievalim0578@gmail.com>
2024-11-25 16:03:33 +0300

    feat: implement schemas for message

---
+++ chat/schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+
+class MessageRead(BaseModel):
+    id: int = Field(..., description="Уникальный ID сообщения")
+    sender_id: int = Field(..., description="Уникальный ID отправителя")
+    recipient_id: int = Field(..., description="Уникальный ID получателя")
+    content: str = Field(..., description="Содержимое сообщения")
+
+
+class MessageCreate(BaseModel):
+    id: int = Field(..., description="Уникальный ID получателя")
+    content: str = Field(..., description="Содержимое сообщения")
